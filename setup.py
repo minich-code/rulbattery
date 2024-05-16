8401649ed145aa81335be14949ca4662cc909b2d
Western Onzere <minichworks@gmail.com>
2024-05-16 10:32:57 +0300

    file and folders

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup 
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+__version__ = "0.0.1"
+SRC_REPO = "Battery-RUL"
+
+setup(
+    name=SRC_REPO,
+    version = __version__,
+    license = 'MIT',
+    url = 'https://github.com/minich-code/rulbattery',
+    download_url = f'https://github.com/minich-code/rulbattery',
+    description='This is a project to Predict Battery Remaining Useful Life',
+    author = 'Western',
+    author_email = 'minichworks@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
+
+
+
+
